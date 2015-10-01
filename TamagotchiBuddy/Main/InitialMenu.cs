26918f5b7dd7cd60da65ce50849efc061dc99ce6
Xasha <gabriela.genewa@gmail.com>
2015-10-01 19:15:15 +0300

    Added quit button.

---
+++ TamagotchiBuddy/Main/InitialMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class InitialMenu : Form
+    {
+        public InitialMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("Do you really want to exit the game?", "Exit?", MessageBoxButtons.YesNo);
+            if (result1 == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
